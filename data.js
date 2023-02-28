843496bdf51d729ee8f05e8db57f5b35f5f2da81
santiruartes <ruartes19@gmail.com>
2023-02-28 17:55:03 -0300

    Se agregó SVG de inicio de sesión y carrito

---
+++ data.js
@@ -0,0 +1,67 @@
+const productList = [
+    {
+        id: 1,
+        name: "planta-1",
+        category: "inside",
+        cost: 5,
+        cardImg: "./asssets/products/inside-product-1.jpg",
+
+    },
+    {
+        id: 2,
+        name: "planta-2",
+        category: "inside",
+        cost: 7,
+        cardImg: "./asssets/products/inside-product-2.jpg",
+
+    },
+    {
+        id: 3,
+        name: "planta-3",
+        category: "inside",
+        cost: 7,
+        cardImg: "./asssets/products/inside-product-3.jpg",
+
+    },
+    {
+        id: 4,
+        name: "planta-4",
+        category: "outside",
+        cost: 8,
+        cardImg: "./asssets/products/outside-product-1.jpg",
+
+    },
+    {
+        id: 5,
+        name: "planta-5",
+        category: "outside",
+        cost: 10,
+        cardImg: "./asssets/products/outside-product-2.jpg",
+
+    },
+    {
+        id: 6,
+        name: "planta-6",
+        category: "pet-friendly",
+        cost: 9.50,
+        cardImg: "./asssets/products/pet-friendly-product-1.jpg",
+
+    },
+    {
+        id: 7,
+        name: "planta-7",
+        category: "pet-friendly",
+        cost: 9.50,
+        cardImg: "./asssets/products/pet-friendly-product-2.jpg",
+
+    },
+    {
+        id: 8,
+        name: "planta-8",
+        category: "pet-friendly",
+        cost: 9.50,
+        cardImg: "./asssets/products/pet-friendly-product-3.jpg",
+
+    },
+
+]   
